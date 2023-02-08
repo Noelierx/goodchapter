f0dae503ae79f3a1a20b70f97e2f51acec518e48
Noëlie <noelieroux11@gmail.com>
2023-02-08 15:59:48 +0100

    feat(*): create new components
    Create LoadingSpinner and Button component

---
+++ src/components/loadingSpinner/LoadingSpinner.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import LoadingSpinner from './LoadingSpinner';
+
+export default {
+    title: 'Components/LoadingSpinner',
+    component: LoadingSpinner,
+    parameters: {
+        docs: {
+            description: {
+                component: 'Display LoadingSpinner when fetching data',
+            },
+        }
+    }
+} as ComponentMeta<typeof LoadingSpinner>;
+
+export const Default: ComponentStory<typeof LoadingSpinner> = () => <LoadingSpinner />;
